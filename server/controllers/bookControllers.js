656184f2e54c0294fbe81a1930ca136251b1625c
Ruts <rutomartin@gmail.com>
2017-08-27 02:59:30 +0300

    debug

--- server/controllers/bookControllers.js
+++ server/controllers/bookControllers.js
@@ -23,11 +23,23 @@ const bookControllers = {
   },
 
   read: (req, res) => {
+    console.log("Im here read");
     Book.find({}, (err, book) => {
       if (err){
         return res.status(404).send(err);
       }
+      return res.status(200).send(book);
+    });
+  },
 
+  update: (req, res) => {
+    console.log("Im here update");
+    Book.findOne({_id: req.params.id}, (err, book) => {
+      if (err){
+        return res.status(500).send(err);
+      }
+
+      console.log("book before is ", book);
       book.bookID = req.body.bookID;
       book.title = req.body.title;
       book.author = req.body.author;
@@ -36,25 +48,17 @@ const bookControllers = {
       book.dateOut = req.body.dateOut;
       book.dateIn = req.body.dateIn;
 
-      newBook.save((err) => {
+      console.log("book after is ", book);
+      book.save((err) => {
         if (err){
           return res.status(500).send(err);
         }
         return res.status(200).send({
           message: "Successfully Updated book",
-          book: book,
+          book,
           body: req.body,
         });
       });
-    });
-  },
-
-  update: (req, res) => {
-    Book.find({_id: req.body._id}, (err, book) => {
-      if (err){
-        return res.status(404).send(err);
-      }
-      return res.status(200).send(book);
     });
   },
 
