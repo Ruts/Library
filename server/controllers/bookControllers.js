2cf3e36e1ba5038e55ef517765bfee9b74485bf4
Ruts <rutomartin@gmail.com>
2017-08-27 02:15:35 +0300

    set update method

--- server/controllers/bookControllers.js
+++ server/controllers/bookControllers.js
@@ -27,12 +27,35 @@ const bookControllers = {
       if (err){
         return res.status(404).send(err);
       }
-      return res.status(200).send(book);
+
+      book.bookID = req.body.bookID;
+      book.title = req.body.title;
+      book.author = req.body.author;
+      book.synopsis = req.body.synopsis;
+      book.borrowed = req.body.borrowed;
+      book.dateOut = req.body.dateOut;
+      book.dateIn = req.body.dateIn;
+
+      newBook.save((err) => {
+        if (err){
+          return res.status(500).send(err);
+        }
+        return res.status(200).send({
+          message: "Successfully Updated book",
+          book: book,
+          body: req.body,
+        });
+      });
     });
   },
 
   update: (req, res) => {
-
+    Book.find({_id: req.body._id}, (err, book) => {
+      if (err){
+        return res.status(404).send(err);
+      }
+      return res.status(200).send(book);
+    });
   },
 
   delete: (req, res) => {
