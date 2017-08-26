43340c92fc74958bff54c132a3728a64cf094e6c
Ruts <rutomartin@gmail.com>
2017-08-26 22:05:42 +0300

    set up database connection

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ const app = require('express')();
 const router = require('./server/routes');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://ruts:ruts@ds159963.mlab.com:59963/library_db');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
